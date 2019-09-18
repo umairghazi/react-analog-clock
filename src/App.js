7ff0816eaca4235b25dcf210d5d5222820328fe5
umairghazi <umairghazi@gmail.com>
2019-09-17 21:50:15 -0400

    Trying my `hand` on analog clock in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
+  state = { hourHandProps: {}, minuteHandProps: {}, secondHandProps: {} };
+  componentDidMount() {
+    setInterval(() => {
+      this.setDate();
+    }, 100);
+  }
+  setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = (seconds / 60) * 360 + 90;
+
+    const mins = now.getMinutes();
+    const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 + 90;
+
+    const hour = now.getHours();
+    const hourDegrees = (hour / 12) * 360 + (mins / 60) * 30 + 90;
+
+    this.setState({
+      hourHandProps: { transform: `rotate(${hourDegrees}deg)` },
+      minuteHandProps: { transform: `rotate(${minsDegrees}deg)` },
+      secondHandProps: { transform: `rotate(${secondsDegrees}deg)` }
+    });
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="container">
+          <div className="clock">
+            <div
+              className="hour-hand hand"
+              style={{ ...this.state.hourHandProps }}
+            />
+            <div
+              className="minute-hand hand"
+              style={{ ...this.state.minuteHandProps }}
+            />
+            <div
+              className="second-hand hand"
+              style={{ ...this.state.secondHandProps }}
+            />
+          </div>
+        </div>
       </div>
     );
   }
